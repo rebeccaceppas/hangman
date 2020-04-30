2b203209019cede4e5030730d3017e145a2a1bf4
Rebecca Ceppas <receppasdc@gmail.com>
2020-04-30 15:30:19 -0400

    hangman including drawing

--- hangman.py
+++ hangman.py
@@ -3,6 +3,35 @@ import random
 words = ['jazz','buzzed','abruptly','absurd','awkward','bagpipes','bikini','blizzard','buffalo','cycle','faking','funny','galaxy','kepler','gossip','injury','jelly','juicy','jogging','kayak','kioks','length','lucky','luxury','matrix','oxygen','pajama','puppy','quizzes','strength','subway','transplant','twelve','syndrome','transcript','unknown','vaporize','vodka','wizard','zipper','zombie','whiskey']
 word = random.choice(words)
 
+#hangman drawing
+
+hangman_parts = [ "head", "left arm", "torso", "right arm", "left leg", "right leg" ]
+
+  
+def draw_hangman(errors):
+
+    hangman_characters = {
+        "head" : "  O",
+        "left arm" : " /",
+        "torso" : "|",
+        "right arm" : "\\",
+        "left leg" : " /",
+        "right leg" : " \\"
+    }
+    hangman_newlines = [ "head", "right arm", "right leg" ]
+
+    output = " _____\n |   |\n | "
+    num_newlines = 0
+    for i in range(errors):
+        output = output + hangman_characters[hangman_parts[i]]
+        if hangman_parts[i] in hangman_newlines:
+            output = output + "\n | "
+            num_newlines = num_newlines + 1
+    for i in range(len(hangman_newlines) - num_newlines):
+        output = output + "\n |"
+    output = output + "____\n\n"
+    print(output)
+
 
 def game(word):
     
@@ -10,15 +39,15 @@ def game(word):
     words = ['JAZZ','BUZZED','ABRUPTLY','ABSURD','AWKWARD','BAGPIPES','BIKINI','BLIZZARD','BUFFALO','CYCLE','FAKING','FUNNY','GALAXY','KEPLER','GOSSIP','INJURY','JELLY','JUICY','JOGGING','KAYAK','KIOSK','LENGTH','LUCKY','LUXURY','MATRIX','OXYGEN','PAJAMA','PUPPY','QUIZZES','STRENGTH','SUBWAY','TRANSPLANT','TWELVE','SYNDROME','TRANSCRIPT','UNKNOWN','VAPORIZE','VODKA','WIZARD','ZIPPER','ZOMBIE','WHISKEY']
     word = random.choice(words)
 
-    print('Okay, this is your word!')
+    print('Okay, this is your word! You have 10 guesses.')
     
-    chances = 10
+    chances = 6
     progress = []
     mistakes = []
     hidden = list('_'*len(word))
     win = False
      
-    print(''.join(hidden))
+    print('\n'+''.join(hidden)+'\n')
     
     while (chances > 0) & (not win):
 
@@ -26,50 +55,56 @@ def game(word):
         
         if (letter.isalpha()) & (letter.upper() in list(word)):
             if letter.upper() in progress:
+                draw_hangman(len(mistakes))
                 print('You already guessed ' + letter.upper())
-                print('You still have ' + str(chances) + ' guesses left')
-                print(''.join(hidden))
+                print('You still have ' + str(chances) + ' guesses left \n')
+                print('\n'+''.join(hidden)+'\n')
             else:
                 progress.append(letter.upper())
                 for n, i in enumerate(list(word)):
                     if i == letter.upper():
                         hidden[n] = letter.upper()
-                print(''.join(hidden))
+                draw_hangman(len(mistakes))
+                print('\n'+''.join(hidden)+'\n')
 
                 if '_' not in hidden:
                     win = True
                     print('You won!')
                 else:
                     print('Good job! ' + letter.upper() + ' is in the word.')
-                    print('You still have ' + str(chances) + ' guesses left')
+                    print('You still have ' + str(chances) + ' guesses left \n')
                 
             
         elif (letter.isalpha()) & (letter.upper() not in list(word)):
             if letter.upper() in mistakes:
+                draw_hangman(len(mistakes))
                 print('You already guessed ' + letter.upper())
-                print('You still have ' + str(chances) + ' guesses left')
+                print('You still have ' + str(chances) + ' guesses left \n')
             else:
                 mistakes.append(letter.upper())
-                print(''.join(hidden))
-                print('Sorry, ' + letter.upper() + ' is not in the word. Try again.')
+                draw_hangman(len(mistakes))
+                print('\n'+''.join(hidden)+'\n')
+                print('Sorry, ' + letter.upper() + ' is not in the word.')
                 chances -= 1
-                print('You now have ' + str(chances) + ' guesses left')
+                print('You now have ' + str(chances) + ' guesses left \n')
             
         else:
-            print('Your input is invalid, try again.')
+            print('Your input is invalid, try again. \n')
 
     if not win:
-        print('You lost! The word was: ' + word )
+        print('You lost! The word was: ' + word)
 
-    repeat = input('Do you want to play again? (Y/N)')
+    repeat = input('Do you want to play again? (Y/N) \n')
     if repeat.upper() == 'Y':
         game(word)
     else:
         print('See you next time')
 
 
-answer = input('Do you want to play Hangman? (Y/N)')
+answer = input('Do you want to play Hangman? (Y/N) \n')
 if answer.upper() == 'Y':
     game(word)
 else:
-    print('See you next time')
+    print('See you next time \n')
+
+
